77920519247a5c027ee7ae59a0b90a02a987564a
João Victor <jotav.lim4@gmail.com>
2024-07-29 15:59:59 -0300

    Lecture01/02 and recitation01 code

---
+++ Lectures_code/rec01_code.py
@@ -0,0 +1,20 @@
+# simple operation between integers
+a = 1
+b = 2
+sum_ab = a + b
+diff_ab = a - b
+
+print(sum_ab)
+print(diff_ab)
+
+# sum/product of strings
+
+string1 = "joao"
+string2 = "victor"
+sum_strings = string1 + ' ' + string2
+sum_digits = str(1) + ' + ' + str(2) + ' = ' + str(3)
+product_strings = 6 * string1 + ' ' + 6 * string2
+
+print(sum_strings)
+print(sum_digits)
+print(product_strings)
