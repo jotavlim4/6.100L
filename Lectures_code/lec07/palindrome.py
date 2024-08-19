e9660d9c3ac520bdf8aa94fe19e7636fdc6a616c
João Victor <jotav.lim4@gmail.com>
2024-08-18 23:18:05 -0300

    Otimizando o código

--- Lectures_code/lec07/palindrome.py
+++ Lectures_code/lec07/palindrome.py
@@ -3,8 +3,7 @@ def palindrome(s):
     input: s is a string
     output: true if s is palindrome and false otherwise
     """
-    invert = s[::-1]
-    return s == invert
+    return s == s[::-1]
     
 print(palindrome('222'))
 print(palindrome('222222'))
