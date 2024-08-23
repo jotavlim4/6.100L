786ee8d665f7e1fed423251b80757977e8140268
João Victor <jotav.lim4@gmail.com>
2024-08-22 23:22:17 -0300

    Modificação na variável da condição do laço while

--- Lectures_code/lec08/find_last.py
+++ Lectures_code/lec08/find_last.py
@@ -7,14 +7,14 @@ def find_last(s, sub):
     if s.find(sub) == -1:
         return None
     i = last_occurence = s.find(sub)
-    while s.find(sub) != -1:
+    while i != -1:
         invert = s[i: i + len(sub)]
         s = s[0:i] + invert[::-1] + s[i + len(sub):]
         last_occurence = i
         i = s.find(sub)
     return last_occurence
            
-answer = find_last('joa é legal, joa é chato', 'pedro') #should return 18
+answer = find_last('joa é legal, joa é chato', 'joa') #should return 18
 print(answer)
 
 
