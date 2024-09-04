8cb498a67784fe8449e9a4e990d751065e77c741
João Victor <jotav.lim4@gmail.com>
2024-09-04 12:17:13 -0300

    Solução do ps1 partes a, b e c

--- Assignments/problem_set_one_code/ps1c.py
+++ Assignments/problem_set_one_code/ps1c.py
@@ -9,11 +9,41 @@ initial_deposit = float(input('Enter the initial deposit: '))
 #########################################################################
 ## Initialize other variables you need (if any) for your program below ##
 #########################################################################
-dream_home_cost = 800000
-down_payment = 0.25 * dream_home_cost
-months = 36
-amount_saved = initial_deposit * (1 + r / 12)**months
+COST_OF_DREAM_HOME = 800000.0
+PORTION_DOWN_PAYMENT = .25
+DOWN_PAYMENT = PORTION_DOWN_PAYMENT * COST_OF_DREAM_HOME
+MONTHS = 36
+EPSILON = 0.0001
+
+def savings_amount(time, initial_deposit, r):
+    savings = initial_deposit
+    for i in range(time):
+        savings += savings*(r/12)
+    return savings
+
+low = 0.0
+high = 1.0
+r = (low + high)/2
+steps = 0
+savings = savings_amount(MONTHS, initial_deposit, r)
 
 ##################################################################################################
 ## Determine the lowest rate of return needed to get the down payment for your dream home below ##
 ##################################################################################################
+if initial_deposit >= DOWN_PAYMENT - 100:
+    r = 0.0
+elif initial_deposit*(1 + 1/12)**MONTHS < DOWN_PAYMENT - 100:
+    r = None
+else:
+    while abs(savings - DOWN_PAYMENT) >= 100:
+        steps += 1
+        savings = savings_amount(MONTHS, initial_deposit, r)
+        if savings < DOWN_PAYMENT:
+            low = r
+        else:
+            high = r
+        r = (low + high)/2
+
+
+print(f'Best savings rate: {r} [or very close to this number]')
+print(f'Steps in bisection search: {steps} [or very close to this number]')
